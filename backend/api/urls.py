e9a859a481ff5cb1cb1011f1c075afb120be1636
Gustavo Pajuelo <gp373@njit.edu>
2025-05-17 13:48:15 -0400

    JWT protected routes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path,include
-from .views import CreateUserView
+from .views import CreateUserView, NoteListCreate
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from . import views
 
 urlpatterns = [
     path('api/user/register/', CreateUserView.as_view(), name='register'),
     path('api/token/', TokenObtainPairView.as_view(), name='get_token'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('api/notes/', views.NoteListCreate.as_view(), name='note-list'),
+    path('api/notes/delete/<int:pk>/', views.NoteDelete.as_view(), name='note-delete'),
 ]
